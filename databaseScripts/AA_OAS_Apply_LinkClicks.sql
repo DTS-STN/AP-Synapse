c6e2d546475686e518ef503be389be48b6bf90de
arpit-dhindsa <90341407+arpit-dhindsa@users.noreply.github.com>
2022-02-02 15:35:37 -0700

    Create AA_OAS_Apply_LinkClicks.sql

---
+++ databaseScripts/AA_OAS_Apply_LinkClicks.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS [dbo].[AA_OAS_Apply_LinkClicks]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[AA_OAS_Apply_LinkClicks]
+( 
+	[page_title_id] [int]  NULL,
+	[page_title] [nvarchar](256)  NULL,
+	[link] [bigint]  NULL,
+	[activity_map_link] [nvarchar](256)  NULL,
+	[year_id] [int]  NULL,
+	[year] [int]  NULL,
+	[link_click_instances] [float]  NULL,
+	[language] [nvarchar](256)  NOT NULL
+)
+WITH
+(
+	DISTRIBUTION = ROUND_ROBIN,
+	CLUSTERED COLUMNSTORE INDEX
+)
+GO
