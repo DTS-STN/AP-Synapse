ca50eb2a12059f20e35ae2e8a7691524a010a03d
arpit-dhindsa <90341407+arpit-dhindsa@users.noreply.github.com>
2022-02-02 15:51:44 -0700

    Create Pop_CountsProjections_Sum.sql

---
+++ databaseScripts/Pop_CountsProjections_Sum.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS [dbo].[Pop_CountsProjections_Sum]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Pop_CountsProjections_Sum]
+( 
+	[geo] [nvarchar](256)  NULL,
+	[statistic] [nvarchar](256)  NULL,
+	[sex] [nvarchar](256)  NULL,
+	[age_group] [nvarchar](256)  NULL,
+	[proj_population] [float]  NULL,
+	[year] [int]  NULL,
+	[change_2022_2068_diff] [float]  NULL,
+	[change_2022_2068_pct] [float]  NULL,
+	[change_2022_2068_max] [float]  NULL,
+	[change_2022_2068_min] [float]  NULL
+)
+WITH
+(
+	DISTRIBUTION = ROUND_ROBIN,
+	CLUSTERED COLUMNSTORE INDEX
+)
+GO
