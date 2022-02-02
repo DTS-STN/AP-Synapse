f09b111d5c8c2f561a6a93bab4bf76af281b87b0
arpit-dhindsa <90341407+arpit-dhindsa@users.noreply.github.com>
2022-02-02 15:36:39 -0700

    Create AA_OAS_Apply_Regions.sql

---
+++ databaseScripts/AA_OAS_Apply_Regions.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS [dbo].[AA_OAS_Apply_Regions]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[AA_OAS_Apply_Regions]
+( 
+	[region] [nvarchar](256)  NULL,
+	[year] [int]  NULL,
+	[unique_visitors_avg] [float]  NULL,
+	[visits_avg] [float]  NULL,
+	[country] [nvarchar](256)  NULL,
+	[yoy_unique_visitors_avg] [float]  NULL,
+	[yoy_visits_avg] [float]  NULL
+)
+WITH
+(
+	DISTRIBUTION = ROUND_ROBIN,
+	CLUSTERED COLUMNSTORE INDEX
+)
+GO
