96eecf212086e17baf7765f2d6514456c85a746e
arpit-dhindsa <90341407+arpit-dhindsa@users.noreply.github.com>
2022-02-08 10:10:31 -0700

    Update OAS_Recipients_ProvType_summary.sql
    remove drop statement

--- databaseScripts/OAS_Recipients_ProvType_summary.sql
+++ databaseScripts/OAS_Recipients_ProvType_summary.sql
@@ -1,11 +1,3 @@
-DROP TABLE IF EXISTS [dbo].[OAS_Recipients_ProvType_summary]
-GO
-
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
 CREATE TABLE [dbo].[OAS_Recipients_ProvType_summary]
 ( 
 	[province] [nvarchar](256)  NULL,
