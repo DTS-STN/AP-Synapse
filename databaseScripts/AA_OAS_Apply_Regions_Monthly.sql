ac559147090e66a78d01162cf31768ca1559de77
arpit-dhindsa <90341407+arpit-dhindsa@users.noreply.github.com>
2022-02-08 10:08:25 -0700

    Update AA_OAS_Apply_Regions_Monthly.sql
    remove drop statement

--- databaseScripts/AA_OAS_Apply_Regions_Monthly.sql
+++ databaseScripts/AA_OAS_Apply_Regions_Monthly.sql
@@ -1,11 +1,3 @@
-DROP TABLE IF EXISTS [dbo].[AA_OAS_Apply_Regions_Monthly]
-GO
-
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
 CREATE TABLE [dbo].[AA_OAS_Apply_Regions_Monthly]
 ( 
 	[region] [nvarchar](256)  NULL,
