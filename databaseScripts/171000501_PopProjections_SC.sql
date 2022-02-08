6d26755295073d4f4f7646b09e06203da5d8684f
arpit-dhindsa <90341407+arpit-dhindsa@users.noreply.github.com>
2022-02-08 10:06:39 -0700

    Update 171000501_PopProjections_SC.sql
    remove drop statement

--- databaseScripts/171000501_PopProjections_SC.sql
+++ databaseScripts/171000501_PopProjections_SC.sql
@@ -1,11 +1,3 @@
-DROP TABLE IF EXISTS [dbo].[171000501_PopProjections_SC]
-GO
-
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
 CREATE TABLE [dbo].[171000501_PopProjections_SC]
 ( 
 	[year] [int]  NULL,
