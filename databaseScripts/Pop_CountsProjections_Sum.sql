d974d935d248e27de2ab4a0cadd458822c5e691c
arpit-dhindsa <90341407+arpit-dhindsa@users.noreply.github.com>
2022-02-08 10:17:36 -0700

    Update Pop_CountsProjections_Sum.sql
    remove drop statement

--- databaseScripts/Pop_CountsProjections_Sum.sql
+++ databaseScripts/Pop_CountsProjections_Sum.sql
@@ -1,11 +1,3 @@
-DROP TABLE IF EXISTS [dbo].[Pop_CountsProjections_Sum]
-GO
-
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
 CREATE TABLE [dbo].[Pop_CountsProjections_Sum]
 ( 
 	[geo] [nvarchar](256)  NULL,
