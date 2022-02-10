bfe6e46c2a91b886f6fd11f80dd6c1d18d922563
arpit-dhindsa <arpit.dhindsa@hrsdc-rhdcc.gc.ca>
2022-02-10 14:31:20 -0700

    add test tables

---
+++ syn-sqlpool-saeb-dev-01/dbo/Tables/Test_Table1.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[test_table1]
+( 
+	[geo] [nvarchar](256)  NULL,
+	[population] [float]  NULL,
+	[year] [int]  NULL
+)
+WITH
+(
+	DISTRIBUTION = ROUND_ROBIN,
+	CLUSTERED COLUMNSTORE INDEX
+)
