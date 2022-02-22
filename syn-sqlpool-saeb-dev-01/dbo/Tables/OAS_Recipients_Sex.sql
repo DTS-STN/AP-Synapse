e6291e7d55ac9a8222e4f05d3e0fce020773d90b
arpit-dhindsa <90341407+arpit-dhindsa@users.noreply.github.com>
2022-02-22 14:12:19 -0700

    Add Synapse Code Base (#2)
    * add sql scripts and dacpac file
    
    * add new line character in sql scripts
    
    * add files
    
    * change_files
    
    * Test commit;
    
    * Removed test table
    
    * Create README.md
    
    * add sqlpackage
    
    * add test tables
    
    * remove sql package
    
    * remove test tables
    
    * add .gitignore
    
    * update .gitignore
    
    Co-authored-by: gopal-kannan <gopal.kannan@hrsdc-rhdcc.gc.ca>

---
+++ syn-sqlpool-saeb-dev-01/dbo/Tables/OAS_Recipients_Sex.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[OAS_Recipients_Sex]
+( 
+	[benefit] [nvarchar](256)  NULL,
+	[sex] [nvarchar](256)  NULL,
+	[period] [nvarchar](256)  NULL,
+	[sex_count] [bigint]  NULL,
+	[year] [int]  NULL,
+	[month] [nvarchar](256)  NULL,
+	[sex_prop] [float]  NULL
+)
+WITH
+(
+	DISTRIBUTION = ROUND_ROBIN,
+	CLUSTERED COLUMNSTORE INDEX
+)
---
+++ syn-sqlpool-saeb-dev-01/dbo/Tables/Pop_EstProjections_SC.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[Pop_EstProjections_SC]
+( 
+	[year] [nvarchar](256)  NULL,
+	[geo] [nvarchar](256)  NULL,
+	[age_group] [nvarchar](256)  NULL,
+	[ann_pop_bothsexes] [nvarchar](256)  NULL,
+	[ann_pop_female] [nvarchar](256)  NULL,
+	[ann_pop_male] [nvarchar](256)  NULL,
+	[statistic] [nvarchar](256)  NULL,
+	[month] [int]  NOT NULL,
+	[date] [nvarchar](256)  NOT NULL,
+	[yoy_pop_chg_bothsexes] [float]  NULL,
+	[yoy_pop_chg_m] [float]  NULL,
+	[yoy_pop_chg_f] [float]  NULL
+)
+WITH
+(
+	DISTRIBUTION = ROUND_ROBIN,
+	CLUSTERED COLUMNSTORE INDEX
+)
